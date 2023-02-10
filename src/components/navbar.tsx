adca0758caf3b88b5723ab174adbaefef662c7e0
ken cargill <101012207+kencargill@users.noreply.github.com>
2023-02-10 15:52:03 -0500

    cleaned up navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,39 +1,62 @@
+import melonPic from '../photos/justMelon.png';
 import { Link } from "react-router-dom";
 
 export default function Navbar () {
     return (
-        <nav className='navbar bg-body-tertiary'>
-             <div>
-                <Link to={'/'}>
-                    <button>Stillwind Farm</button>
-                </Link>
-            </div>
-            <div>
-                <Link to={'/csa'}>
-                    <button>CSA</button>
-                </Link>
-            </div>
-            <div>
-                <Link to={'/marketcard'}>
-                    <button>Market Card</button>
-                </Link>
-            </div>
-            <div>
-                <Link to={'/about'}>
-                    <button>About</button>
-                </Link>
-            </div>
-            <div>
-                <Link to={'/contact'}>
-                    <button>Contact Us</button>
-                </Link>
-            </div>
-            <div>
-                <Link to={'/shop'}>
-                    <button>shop</button>
-                </Link>
-            </div>
-        {/* <a> cart </a> */}
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+
+        <a className="navbar-brand" href="/">
+            <img src={melonPic} alt="melon" width="50"/>
+        </a>
+
+        <a className="navbar-brand" href="/">Stillwind Farm</a>
+
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+
+            <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="/csa">CSA</a>
+            </li>
+
+            <li className="nav-item">
+                <a className="nav-link" href="/marketcard">Market Card</a>
+            </li>
+
+            <li className="nav-item">
+                <a className="nav-link" href="/about">About</a>
+            </li>
+
+            <li className="nav-item">
+                <a className="nav-link" href="/contact">Contact Us</a>
+            </li>
+
+            <li className="nav-item">
+                <a className="nav-link" href="/shop">Shop</a>
+            </li>
+
+            {/* <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown
+                </a>
+                <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">Action</a></li>
+                <li><a className="dropdown-item" href="#">Another action</a></li>
+                <li><hr className="dropdown-divider"/></li>
+                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+            </li> */}
+
+            </ul>
+
+        </div>
+
+        </div>
         </nav>
     )
 }
