f50690420c6d125221f2c1949970540773433a9f
ken cargill <101012207+kencargill@users.noreply.github.com>
2023-03-01 09:00:32 -0500

    corrected import of bootstrap

--- src/components/homePage.tsx
+++ src/components/homePage.tsx
@@ -1,6 +1,6 @@
 export default function HomePage () {
     return (
-        <div>
+        <div className="container">
             <h2>Welcome to Stillwind Farm!</h2>
             <p>We are a 1/2 acre, low-till, Certified Naturally Grown vegetable farm located in Belding, just outside of the Grand Rapids area. Shop our CSA shares and/or Market Cards for the 2022 season!</p>
             <p>stay in the loop</p>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,57 +5,57 @@ export default function Navbar () {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-{/* 
-        <a className="navbar-brand" href="/">
-            <img src={melonPic} alt="melon" width="50"/>
-        </a> */}
+            {/* <div className='row'> */}
+                {/* <a className="navbar-brand" href="/">
+                    <img src={melonPic} alt="melon" width="50"/>
+                </a> */}
 
-        <a className="navbar-brand" href="/">Stillwind Farm</a>
+                <a className="navbar-brand" href="/">Stillwind Farm</a>
 
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-        </button>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
 
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
-            <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="/csa">CSA</a>
-            </li>
+                    <li className="nav-item">
+                        <a className="nav-link" aria-current="page" href="/csa">CSA</a>
+                    </li>
 
-            <li className="nav-item">
-                <a className="nav-link" href="/marketcard">Market Card</a>
-            </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/marketcard">Market Card</a>
+                    </li>
 
-            <li className="nav-item">
-                <a className="nav-link" href="/about">About</a>
-            </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/about">About</a>
+                    </li>
 
-            <li className="nav-item">
-                <a className="nav-link" href="/contact">Contact Us</a>
-            </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/contact">Contact Us</a>
+                    </li>
 
-            <li className="nav-item">
-                <a className="nav-link" href="/shop">Shop</a>
-            </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/shop">Shop</a>
+                    </li>
 
-            {/* <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-                </a>
-                <ul className="dropdown-menu">
-                <li><a className="dropdown-item" href="#">Action</a></li>
-                <li><a className="dropdown-item" href="#">Another action</a></li>
-                <li><hr className="dropdown-divider"/></li>
-                <li><a className="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-            </li> */}
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown
+                        </a>
+                        <ul className="dropdown-menu">
+                        <li><a className="dropdown-item" href="#">Action</a></li>
+                        <li><a className="dropdown-item" href="#">Another action</a></li>
+                        <li><hr className="dropdown-divider"/></li>
+                        <li><a className="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
 
-            </ul>
+                    </ul>
 
-        </div>
-
+                </div>
+            {/* </div> */}
         </div>
         </nav>
     )
