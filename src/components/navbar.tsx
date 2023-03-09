38d673e3a243777ab277cbed2dd1ebf1601f8a08
ken cargill <101012207+kencargill@users.noreply.github.com>
2023-03-09 12:42:31 -0500

    styles added, home page formatting

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,4 @@
-import melonPic from '../photos/justMelon.png';
+import brand from '../styles/photos/stliiwind-farm-clay.png'
 import { useState } from 'react';
 
 export default function Navbar () {
@@ -8,22 +8,22 @@ export default function Navbar () {
         const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
 
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand-lg ">
         <div className="container-fluid">
             {/* <div className='row'> */}
-                {/* <a className="navbar-brand" href="/">
-                    <img src={melonPic} alt="melon" width="50"/>
-                </a> */}
+                <a className="navbar-brand" href="/">
+                    <img src={brand} alt="melon" width="250"/>
+                </a> 
 
-                <a className="navbar-brand ps-3" href="/">Stillwind Farm</a>
+                {/* <a className="navbar-brand ps-3" href="/">Stillwind Farm</a> */}
 
                 <button className="navbar-toggler m-1" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded={isNavCollapsed ? true : false} aria-label="Toggle navigation" onClick={handleNavCollapse}>
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse text-end`} id="navbarContent">
+                <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse col-md-10 row`} id="navbarContent">
 
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 px-3">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 px-3 align-self-end">
 
                     <li className="nav-item">
                         <a className="nav-link" aria-current="page" href="/csa">CSA</a>
--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -6,13 +6,13 @@ export default function HomePage () {
     return (
         <div className="d-grid home-page container-fluid justify-content-center">
             <div className="row">
-                <div className="col ">
+                <div className="row col-md">
                     <img className="img align-middle" width="100%" src={couplePic} alt=""/>
                 </div>
-                <div className="col">
-                    <h2 className="gap-2 col-6 mx-auto">Welcome to Stillwind Farm!</h2>
+                <div className="row col-md">
+                    <h2 className="gap-2 col-md-6 mx-auto">Welcome to Stillwind Farm!</h2>
                     <p className="gap-2 col-10 mx-auto">We are a 1/2 acre, low-till, Certified Naturally Grown vegetable farm located in Belding, just outside of the Grand Rapids area. Shop our CSA shares and/or Market Cards for the 2022 season!</p>
-                    <div className="mx-auto">
+                    <div className="gap-2 col-6 mx-auto">
                         <Link to={'/contact'} >
                                 <button type="button" className="btn btn-success">stay in the loop</button>
                         </Link>
