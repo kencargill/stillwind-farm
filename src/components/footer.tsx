0183a9554df152848ab7cd60712291959c34a96f
ken cargill <101012207+kencargill@users.noreply.github.com>
2023-03-14 12:48:17 -0400

    footer styling

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,16 +1,22 @@
 import { Link } from "react-router-dom";
+import cloud from "../styles/photos/cloud.png"
 
 export default function Footer () {
     return(
-        <div className="container-fluid d-flex flex-row-reverse bg-f">
-            <div className="mx-auto mt-3 z-3 position-sticky bottom-0">
+        <div className="container-fluid d-flex flex-row-reverse bg-f justify-content-start ms-0">
+            <div className=" position-sticky bottom-0 col-12 col-md-6">
                 <form>
-                    <label htmlFor="email">sign up for our newsletter</label>
-                    <input id="email" type="text" placeholder="enter your email" />
-
-                        {/* <Link to={'/contact'}> */}
-                                <button type="button" className="btn btn-success m-2">stay in the loop</button>
-                        {/* </Link> */}
+                    <div className="row justify-content-end mt-2 text-end">
+                        <img src={cloud} className="col-2 object-fit-scale" height="50"/>
+                        <img src={cloud} className="col-2 object-fit-scale" height="25"/>
+                        <img src={cloud} className="col-2 object-fit-scale" height="50"/>
+                        <img src={cloud} className="col-2 object-fit-scale" height="25"/>
+                        <label htmlFor="email" className="form-label col mt-2 text-success">want our newsletter?</label>
+                    </div>
+                    <div className="row justify-content-end mb-2 d-flex">
+                        <input className="col form-control" id="email" type="email" placeholder="enter your email" />
+                        <button type="button" className="col-md-3 btn btn-success">stay in the loop</button>
+                    </div>
 
                 </form>
                     </div>
